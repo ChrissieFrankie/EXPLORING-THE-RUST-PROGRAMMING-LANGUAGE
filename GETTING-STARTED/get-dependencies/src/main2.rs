930496d668215c90294aa844fd7e284f1ff46f39
Christopher Francisco <cjfg00gl3@gmail.com>
2025-08-20 17:46:37 -0400

    CREATE main2.rs with example usage of rustc for single file execution

---
+++ GETTING-STARTED/get-dependencies/src/main2.rs
@@ -0,0 +1,6 @@
+fn main()
+{
+    println!("hey there, now if you just want to run a single rs file instead of a project just use rustc");
+    println!("---------------------------------powershell------------------------------------");
+    println!("PS C:> runc main2.rs");
+}
